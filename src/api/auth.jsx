ff3c9d2d817e6990501bcf52041f919694c2d820
Niaal-B <nihnihal504@gmail.com>
2025-10-11 12:11:15 +0530

    feat: register user and login user created

---
+++ src/api/auth.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8000"; 
+
+export const registerUser = async (userData) => {
+  const response = await axios.post(`${API_BASE_URL}/auth/register`, userData);
+  return response.data;
+};
+
+export const loginUser = async (credentials) => {
+  const response = await axios.post(`${API_BASE_URL}/auth/login`, credentials);
+  return response.data;
+};
